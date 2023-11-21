57f99b13bac1ecfb14d73812fdd082930497f8ad
Ayman <a.m_ayman9@outlook.com>
2023-11-21 16:01:10 +0200

    Refactor Url at BillingAddressController, ShippingAddressController

--- src/main/java/com/example/shopapi/controller/BillingAddressController.java
+++ src/main/java/com/example/shopapi/controller/BillingAddressController.java
@@ -7,16 +7,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/billingAddress")
 public class BillingAddressController {
     private final BillingAddressService billingAddressService;
 
-    @GetMapping("/customer/{customerId}")
+    @GetMapping("/api/v1/customer/{customerId}/billingAddress")
     public ResponseEntity<BillingAddressDto> getBillingAddressByCustomerId(@PathVariable int customerId) {
         return new ResponseEntity<>(billingAddressService.getBillingAddressByCustomerId(customerId), HttpStatus.OK);
     }
--- src/main/java/com/example/shopapi/controller/ShippingAddressController.java
+++ src/main/java/com/example/shopapi/controller/ShippingAddressController.java
@@ -7,16 +7,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/shippingAddress")
 public class ShippingAddressController {
     private final ShippingAddressService shippingAddressService;
 
-    @GetMapping("/customer/{customerId}")
+    @GetMapping("/api/v1/customer/{customerId}/shippingAddress")
     public ResponseEntity<ShippingAddressDto> getShippingAddressByCustomerId(@PathVariable int customerId) {
         return new ResponseEntity<>(shippingAddressService.getShippingAddressByCustomerId(customerId), HttpStatus.OK);
     }
